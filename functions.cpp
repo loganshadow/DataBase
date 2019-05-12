cc9836ccf6a58982372b5c208bb9af00f569c854
loganshadow <efremov.m.s@edu.mirea.ru>
2019-05-13 01:42:26 +0300

    fin

--- functions.cpp
+++ functions.cpp
@@ -44,58 +44,61 @@ M1: i = -1;
     cout << "6)Change post" << endl;
     cout << "0)Back to main menu" << endl;
     cout << "---------------------------------------------------------------------" << endl;
-    while(i < 0 || i > 6) {
+    while(i < 0 || i > 6){
         cin >> i;
-        switch (i) {
-            case 1: {
-                system("cls");
-                cout << "Enter new name" << endl;
-                cin >> temp;
-                employ[j].setName(temp);
-                goto M1;
-            }
-            case 2: {
-                system("cls");
-                cout << "Enter new surname" << endl;
-                cin >> temp;
-                employ[j].setSname(temp);
-                cout << "Name has been changed!" << endl;
-                goto M1;
-            }
-            case 3: {
-                system("cls");
-                cout << "Enter new email" << endl;
-                cin >> temp;
-                employ[j].setEmail(temp);
-                cout << "Surname has been changed!" << endl;
-                goto M1;
-            }
-            case 4: {
-                system("cls");
-                cout << "Enter new phone" << endl;
-                cin >> temp;
-                employ[j].setPhone(temp);
-                cout << "Phone has been changed!" << endl;
-                goto M1;
-            }
-            case 5: {
-                system("cls");
-                cout << "Enter new passport" << endl;
-                cin >> temp;
-                employ[j].setPassport(temp);
-                cout << "Passport has been changed!" << endl;
-                goto M1;
-            }
-            case 6: {
-                system("cls");
-                cout << "Enter new post" << endl;
-                cin >> temp;
-                employ[j].setPost(temp);
-                cout << "Post has been changed!" << endl;
-                goto M1;
-            }
+        if (i < 0 || i > 6)
+            cout << "Try again" << endl;
+    }
+    switch (i) {
+        case 1: {
+            system("cls");
+            cout << "Enter new name" << endl;
+            cin >> temp;
+            employ[j].setName(temp);
+            goto M1;
+        }
+        case 2: {
+            system("cls");
+            cout << "Enter new surname" << endl;
+            cin >> temp;
+            employ[j].setSname(temp);
+            cout << "Name has been changed!" << endl;
+            goto M1;
+        }
+        case 3: {
+            system("cls");
+            cout << "Enter new email" << endl;
+            cin >> temp;
+            employ[j].setEmail(temp);
+            cout << "Surname has been changed!" << endl;
+            goto M1;
+        }
+        case 4: {
+            system("cls");
+            cout << "Enter new phone" << endl;
+            cin >> temp;
+            employ[j].setPhone(temp);
+            cout << "Phone has been changed!" << endl;
+            goto M1;
+        }
+        case 5: {
+            system("cls");
+            cout << "Enter new passport" << endl;
+            cin >> temp;
+            employ[j].setPassport(temp);
+            cout << "Passport has been changed!" << endl;
+            goto M1;
+        }
+        case 6: {
+            system("cls");
+            cout << "Enter new post" << endl;
+            cin >> temp;
+            employ[j].setPost(temp);
+            cout << "Post has been changed!" << endl;
+            goto M1;
         }
     }
+
 }
 
 void AddEmployee(std::vector <employee> &employ){
@@ -111,11 +114,19 @@ void DisplayAllandInfo(std::vector <employee> &employ){
      }
     cout << "---------------------------------------------------------------------" << endl;
     cout << "Enter number of employee to see more info" << endl;
-     int j;
-     cin >> j;
-     j--;
-     employ[j].get_employee();
-    cout << "---------------------------------------------------------------------" << endl;
+    cout << "Type 0 to exit to main menu" << endl;
+     int j = -1;
+    while(j < 0 || j > employ.size()){
+        cin >> j;
+        if (j < 0 || j > employ.size())
+            cout << "Try again" << endl;
+    }
+    if(j != 0) {
+        j--;
+        employ[j].get_employee();
+        cout << "---------------------------------------------------------------------" << endl;
+        system("pause");
+    }
 }
 
 void DisplayAll(std::vector <employee> &employ) {
@@ -128,13 +139,17 @@ void DisplayAll(std::vector <employee> &employ) {
 
 void DeleteEmployee(std::vector <employee> &employ){
      cout << "Enter number of employee for eraising from the base" << endl;
-     int i;
+     int i = -1;
      char c;
-     cin >> i;
+    while(i < 0 || i > employ.size()){
+        cin >> i;
+        if (i < 0 || i > employ.size())
+            cout << "Try again" << endl;
+    }
      i--;
      cout << "Do you really want to delete this employee from the base?[y/n]" << endl;
      cin >> c;
-     if(c == 'y') {
+     if(c == 'y' || c == '1') {
          auto p = employ.begin();
          p += i;
          employ.erase(p);
--- main.cpp
+++ main.cpp
@@ -11,11 +11,16 @@ int main() {
     employee workerBuff;
     ifstream fin("DataBase.txt");
     vector <employee> employee;
+    int s = 0;
     while(!fin.eof( )){
         fin >> workerBuff;
         employee.push_back(workerBuff);
+        s++;
     }
     fin.close();
+    auto p = employee.begin();
+    p += s;
+    employee.erase(p);
 X1: int menu = menuPage1();
     if(menu == 1) {//OPTION ONE
         system("cls");
@@ -23,34 +28,40 @@ X1: int menu = menuPage1();
         system("pause");
         goto X1;
     }
-    if(menu == 2){//OPTION TWO
-        system("cls");
-        DisplayAll(employee);
-        menuPage2(employee);
-        goto X1;
-    }
-    if(menu == 3){//OPTION THREE
-        system("cls");
-        DisplayAll(employee);
-        DeleteEmployee(employee);
-        system("pause");
-        goto X1;
-    }
-    if(menu == 4){//OPTION FOUR
+    if(!employee.empty()){
+        if(menu == 2){//OPTION TWO
+            system("cls");
+            DisplayAll(employee);
+            menuPage2(employee);
+            goto X1;
+        }
+        if(menu == 3){//OPTION THREE
+            system("cls");
+            DisplayAll(employee);
+            DeleteEmployee(employee);
+            system("pause");
+            goto X1;
+        }
+        if(menu == 4){//OPTION FOUR
 
-    }
-    if(menu == 5){//OPTION FIVE
+        }
+        if(menu == 5){//OPTION FIVE
+            system("cls");
+            DisplayAllandInfo(employee);
+            goto X1;
+        }
+    } else {
         system("cls");
-        DisplayAllandInfo(employee);
+        cout << "Base is empty! Add workers" << endl;
+        cout << "---------------------------------------------------------------------" << endl;
         system("pause");
         goto X1;
     }
-/*    ofstream fout("DataBase.txt");
+
+    ofstream fout("DataBase.txt");
     for (int i = 0; i < employee.size(); i++)
-        cout << employee[i];
+        fout << employee[i];
     fout.close();
     system("pause");
-    */
-    employee.clear();
     return 0;
 }
